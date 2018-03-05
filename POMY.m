346df192a08f1104fc7f704ffc108ec976d61f6c
IncurableCodeDreamer <kla.96.kn@gmail.com>
2018-03-05 15:14:49 +0100

    Add scaling to pictures

--- POMY.m
+++ POMY.m
@@ -21,7 +21,7 @@ function varargout = POMY(varargin)
 % See also: GUIDE, GUIDATA, GUIHANDLES
 
 % Edit the above text to modify the response to help POMY
-% Last Modified by GUIDE v2.5 26-Feb-2018 14:53:04
+% Last Modified by GUIDE v2.5 05-Mar-2018 14:41:01
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -77,13 +77,26 @@ function LoadBtn_Callback(hObject, eventdata, handles)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 global obraz 
+set(handles.ScalNumber,'String','');
 sciezka=uigetfile('*.jpg');
 obraz=imread(sciezka);
 axes(handles.LoadedPicture)
 imshow(obraz);
-%[x,y] = ginput(6);
-%axes(handles.PictureWithPoints);
-%plot(x,y,'*');
+h = msgbox('Zaznacz dwa punkty, a nastêpnie wpisz rzeczywist± odleg³o¶æ miêdzy nimi. Potem naci¶nij przycisk SKALUJ','Skalowanie','help');
+waitfor(h);
+
+[x1,y1] = ginput(1);
+hold(handles.LoadedPicture,'on');
+plot(x1,y1,'r*');
+[x2,y2] = ginput(1);
+hold(handles.LoadedPicture,'on');
+plot(x2,y2,'r*');
+
+handles.x1=x1;
+handles.x2=x2;
+handles.y1=y1;
+handles.y2=y2;
+guidata(hObject,handles)
 
 % --- Executes on button press in ReturnBtn.
 function ReturnBtn_Callback(hObject, eventdata, handles)
@@ -97,3 +110,58 @@ function CountBtn_Callback(hObject, eventdata, handles)
 % hObject    handle to CountBtn (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
+
+
+
+function ScalNumber_Callback(hObject, eventdata, handles)
+% hObject    handle to ScalNumber (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of ScalNumber as text
+%        str2double(get(hObject,'String')) returns contents of ScalNumber as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function ScalNumber_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to ScalNumber (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in ScalBtn.
+function ScalBtn_Callback(hObject, eventdata, handles)
+% hObject    handle to ScalBtn (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+x = str2num(get(handles.ScalNumber,'String')); %edit1 being Tag of ur edit box
+ if isempty(x)
+     h = msgbox('Wprowad¼ warto¶æ liczbow±!','B³±d','error');
+     set(handles.ScalNumber,'String','');
+ end
+ if (x<=0)
+     h = msgbox('Wprowad¼ warto¶æ liczbow±!','B³±d','error');
+     set(handles.ScalNumber,'String','');
+     x=[];
+ end
+ 
+ x1=handles.x1;
+ x2=handles.x2;
+ y1=handles.y1;
+ y2=handles.y2;
+ odleglosc = sqrt((x1-x2)^2+(y1-y2)^2);
+ wartosc_piksela=x/odleglosc;
+
+%[x,y] = ginput(6);
+%hold(handles.LoadedPicture,'on');
+%plot(x,y,'ro');
+%hold off
+%axes(handles.PictureWithPoints);
+%plot(x,y,'*');
