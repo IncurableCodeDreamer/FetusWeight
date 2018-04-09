88521b90094d514ccc27a0f35db0ab3af65314b9
IncurableCodeDreamer <kla.96.kn@gmail.com>
2018-04-09 19:07:51 +0200

    finished

--- POMY.m
+++ POMY.m
@@ -21,7 +21,7 @@ function varargout = POMY(varargin)
 % See also: GUIDE, GUIDATA, GUIHANDLES
 
 % Edit the above text to modify the response to help POMY
-% Last Modified by GUIDE v2.5 05-Mar-2018 14:41:01
+% Last Modified by GUIDE v2.5 26-Mar-2018 14:28:01
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -79,6 +79,12 @@ function LoadBtn_Callback(hObject, eventdata, handles)
 global obraz 
 global rozmiar_obrazu;
 cla(handles.PictureWithPoints,'reset');
+set(handles.ScalNumber, 'String', '');
+set(handles.text9, 'String', '');
+set(handles.text10, 'String', '');
+set(handles.text11, 'String', '');
+set(handles.text12, 'String', '');
+
 set(handles.ScalNumber,'String','');
 sciezka=uigetfile('*.jpg');
 obraz=imread(sciezka);
@@ -110,14 +116,92 @@ function ReturnBtn_Callback(hObject, eventdata, handles)
 % hObject    handle to ReturnBtn (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
+global obraz;
+cla(handles.PictureWithPoints,'reset');
+set(handles.LoadedPicture,'Units','pixels');
+ resizePos = get(handles.LoadedPicture,'Position');
+ obraz= imresize(obraz, [resizePos(3) resizePos(3)]);
+ axes(handles.LoadedPicture);
+ imshow(obraz);
+set(handles.ScalNumber, 'String', '');
+set(handles.text9, 'String', '');
+set(handles.text10, 'String', '');
+set(handles.text11, 'String', '');
+set(handles.text12, 'String', '');
+ 
+  hFigure = figure;
+ set(hFigure, 'MenuBar', 'none');
+ set(hFigure, 'ToolBar', 'none');
+ set(hFigure, 'Name','Instrukcja zaznaczania punktów na obrazie usg')
+ Image = imread('instrukcja.jpg');
+ imshow(Image,'Border','tight');
+ title('Zaznacz odpowiednie punkty w przedstawionej kolejno¶ci');
 
-
+ waitfor(hFigure);
+ [x,y] = ginput(1); 
+ hold(handles.LoadedPicture,'on');
+ plot(x,y,'go');
+ [x1,y1] = ginput(1);
+ hold(handles.LoadedPicture,'on');
+ plot(x1,y1,'go');
+ [x2,y2] = ginput(1); 
+ hold(handles.LoadedPicture,'on');
+ plot(x2,y2,'go');
+ [x3,y3] = ginput(1); 
+ hold(handles.LoadedPicture,'on');
+ plot(x3,y3,'go');
+ [x4,y4] = ginput(1);
+ hold(handles.LoadedPicture,'on');
+ plot(x4,y4,'go');
+ [x5,y5] = ginput(1);
+ hold(handles.LoadedPicture,'on');
+ plot(x5,y5,'go');
+ 
+ wektor_pkt1=[x,x1,x2,x3,x4,x5;y,y1,y2,y3,y4,y5]
+ wektor_pkt1_1=[x x1; y y1];
+ wektor_pkt1_2=[x2 x3; y2 y3];
+ wektor_pkt1_3=[x4 x5; y4 y5];
+ hold off
+ axes(handles.PictureWithPoints);
+ points_plot=plot(wektor_pkt1(1,:),wektor_pkt1(2,:),'b*');
+ direction = [1 0 0];
+rotate(points_plot,direction,180)
+hold on
+points_plot1=plot(wektor_pkt1_1(1,:),wektor_pkt1_1(2,:),'r');
+ direction = [1 0 0];
+rotate(points_plot1,direction,180)
+points_plot2=plot(wektor_pkt1_2(1,:),wektor_pkt1_2(2,:),'r');
+ direction = [1 0 0];
+rotate(points_plot2,direction,180)
+points_plot3=plot(wektor_pkt1_3(1,:),wektor_pkt1_3(2,:),'r');
+ direction = [1 0 0];
+rotate(points_plot3,direction,180)
+ grid on;
+ handles.x=x;
+handles.x1=x1;
+handles.y=y;
+handles.y1=y1;
+handles.x3=x3;
+handles.x2=x2;
+handles.y3=y3;
+handles.y2=y2;
+handles.x4=x4;
+handles.x5=x5;
+handles.y4=y4;
+handles.y5=y5;
+guidata(hObject,handles)
+guidata(hObject,handles)
+ 
 % --- Executes on button press in CountBtn.
 function CountBtn_Callback(hObject, eventdata, handles)
 % hObject    handle to CountBtn (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 global wartosc_piksela;
+global HC;
+global AC;
+global FL;
+global BW;
 x1=handles.x1;
  x2=handles.x2;
  y1=handles.y1;
@@ -130,17 +214,16 @@ x1=handles.x1;
  x5=handles.x5;
  y4=handles.y4;
  y5=handles.y5;
- HC=sqrt((x-x1)^2+(y-y1)^2)*wartosc_piksela*pi
- AC=sqrt((x2-x3)^2+(y2-y3)^2)*wartosc_piksela*pi
- FL=sqrt((x4-x5)^2+(y4-y5)^2)*wartosc_piksela
- BW=10^(1.304+(0.05281*AC)+(0.1938*FL)-(0.004*(AC*FL)))
+ HC=sqrt((x-x1)^2+(y-y1)^2)*wartosc_piksela*pi;
+ AC=sqrt((x2-x3)^2+(y2-y3)^2)*wartosc_piksela*pi;
+ FL=sqrt((x4-x5)^2+(y4-y5)^2)*wartosc_piksela;
+ BW=10^(1.304+(0.05281*AC)+(0.1938*FL)-(0.004*(AC*FL)));
  j='cm';
  j2='g';
- set(handles.text10,'String',strcat(num2str(HC),j))
- set(handles.text9,'String',strcat(num2str(AC),j))
- set(handles.text11,'String',strcat(num2str(FL),j))
- set(handles.text12,'String',strcat(num2str(BW),j2))
-
+ set(handles.text10,'String',strcat(num2str(HC),j));
+ set(handles.text9,'String',strcat(num2str(AC),j));
+ set(handles.text11,'String',strcat(num2str(FL),j));
+ set(handles.text12,'String',strcat(num2str(BW),j2));
 
 function ScalNumber_Callback(hObject, eventdata, handles)
 % hObject    handle to ScalNumber (see GCBO)
@@ -149,7 +232,6 @@ function ScalNumber_Callback(hObject, eventdata, handles)
 
 % Hints: get(hObject,'String') returns contents of ScalNumber as text
 %        str2double(get(hObject,'String')) returns contents of ScalNumber as a double
-
 
 % --- Executes during object creation, after setting all properties.
 function ScalNumber_CreateFcn(hObject, eventdata, handles)
@@ -192,7 +274,7 @@ x = str2num(get(handles.ScalNumber,'String')); %edit1 being Tag of ur edit box
  y2=handles.y2;
  odleglosc = sqrt((x1-x2)^2+(y1-y2)^2)
  wartosc_piksela=x/odleglosc
- end 
+
  
  set(handles.LoadedPicture,'Units','pixels');
  resizePos = get(handles.LoadedPicture,'Position');
@@ -262,3 +344,19 @@ handles.y4=y4;
 handles.y5=y5;
 guidata(hObject,handles)
 guidata(hObject,handles)
+end 
+
+% --- Executes on button press in statBtn.
+function statBtn_Callback(hObject, eventdata, handles)
+% hObject    handle to statBtn (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+if get(hObject, 'value')
+    POMY_2
+else
+    try
+        close POMY_2
+    catch
+    end
+end
